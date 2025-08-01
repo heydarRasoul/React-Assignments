b15862dd794a3023a5ce6aad310bef49b8427336
Heydar Rasoulpouranaei <heydarrasoulpour@ddevpipeline.com>
2025-08-01 11:06:23 -0600

    fixing the weather widget

--- react-widget-2/src/components/pages/Weather.jsx
+++ react-widget-2/src/components/pages/Weather.jsx
@@ -2,19 +2,33 @@ import { useEffect, useState } from "react";
 
 export default function Weather() {
   const [temp, setTemp] = useState(null);
+
   useEffect(() => {
-    fetch("https://www.metaweather.com/api/location/search/?query=London")
+    fetch("https://api.weather.gov/points/40.31,-111.71")
       .then((res) => res.json())
       .then((data) => {
-        setTemp(data.main.temp);
+        const forecastUrl = data.properties.forecast;
+        return fetch(forecastUrl);
+      })
+      .then((res) => res.json())
+      .then((forecastData) => {
+        const temperature = forecastData.properties.periods[0].temperature;
+        setTemp(temperature);
       })
       .catch((err) => console.error("Fetch error:", err));
   }, []);
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
       <h1>Weather in Utah</h1>
-      {temp !== null ? <h2>Temperature: {temp}°C</h2> : <p>Loading...</p>}
+      {temp !== null ? <h2>Temperature: {temp}°F</h2> : <p>Loading...</p>}
     </div>
   );
 }
